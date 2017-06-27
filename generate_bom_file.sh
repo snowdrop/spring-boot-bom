02171120c9317b703002711451aa38811786198d
dabou <ch007m@gmail.com>
2017-06-27 13:45:20 +0200

    Fix error

--- generate_bom_file.sh
+++ generate_bom_file.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-export CURRENT=$(pwd)
 PROJECT_DIR=spring-boot-parent
 VERSION=${1:-2.11}
 
+rm -rf  target && mkdir target && cd target
+
 if [ -d $PROJECT_DIR ]; then
  echo "## Deleting $PROJECT_DIR directory ...."
- rmdir $PROJECT_DIR
+ rm -rf $PROJECT_DIR
 fi
 
-git clone https://github.com/snowdrop/spring-boot-parent.git && cd $PROJECT_DIR
+git clone git@github.com:snowdrop/spring-boot-parent.git && cd $PROJECT_DIR
 curl -O http://central.maven.org/maven2/org/commonjava/maven/ext/pom-manipulation-cli/$VERSION/pom-manipulation-cli-$VERSION.jar
 
 git checkout -b release
 java -jar pom-manipulation-cli-$VERSION.jar
 git commit -m "Update pom file" pom.xml
-git push
+git push --set-upstream origin release
+
+cd ~
+
